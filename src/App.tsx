61e0bfd3bb2188254e925f95146a1dc26866fa66
JulianTorrest <43328220+JulianTorrest@users.noreply.github.com>
2024-06-26 20:15:20 -0500

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SaleOrderItemComponent from './components/SaleOrderItem'
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <SaleOrderItemComponent />
+    </div>
+  )
+}
+
+export default App
